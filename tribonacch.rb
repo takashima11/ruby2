76099f67e7be14430d66f063aea49864d198423d
takashima11 <129072192+takashima11@users.noreply.github.com>
2023-12-31 06:12:20 +0000

    トリボナッチ数列2

--- tribonacch.rb
+++ tribonacch.rb
@@ -16,3 +16,46 @@ puts "数を出したいのは何項目ですか？"
 n = gets.to_i
 
 puts "#{n}項目の数字は#{tribonacch(n)}"
+
+
+
+
+# 1,3,7,11,21,39...
+# 50番個目にある数字は何ですか？
+
+puts "求めたい数字を入力してください"
+puts "1つ目の数字"
+a = gets.to_i
+puts "2つ目の数字"
+b = gets.to_i
+puts "3つ目の数字"
+c = gets.to_i
+puts "何番目の数字を求めますか？"
+t = gets.to_i
+
+n = 0
+while n < (t - 3)
+  d = a + b + c
+  a = b
+  b = c
+  c = d
+  n += 1
+end
+
+puts "#{t}番目の数は#{c}です"
+
+
+# # ふつうに
+# a = [1, 3, 5]
+# c = 0
+# while c < 47
+#   a.push(a[c] + a[c + 1] + a[c + 2])
+#   c += 1
+# end
+
+# puts a[-1]
+
+
+a, b, c = 1, 3, 7
+(50 - 1).times {a, b, c = b, c, a + b + c }
+puts "{a}"
