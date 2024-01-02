0da5094d0672b4062f6879dcfa6c519201ece1fb
takashima11 <129072192+takashima11@users.noreply.github.com>
2024-01-02 11:24:00 +0000

    途中まで

---
+++ team-lab.rb
@@ -0,0 +1,23 @@
+a = gets.to_i
+
+i = 0
+while i < 30
+  b = (a + (a / 2)).floor
+  a = b
+  i += 1
+end
+
+puts "#{b}"
+
+
+# --------------------
+
+n = "8".to_i = "SNOWMAN"
+a = "(1..100000)"
+def count_number(number)
+  list = []
+  for i in 1.upto(number) do
+
+  end
+  list.sum{|i| i.to_s.count("8")}
+end
